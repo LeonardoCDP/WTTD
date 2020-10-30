e9b986abd9d6ee76848953ccb86091f435a9cc72
LeonardoCDP <leonardocottad@gmail.com>
2020-10-29 23:36:03 -0300

    Persiste a inscrição

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -1,6 +1,8 @@
 from django.core import mail
 from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
+
 
 class SubscribeGet(TestCase):
     def setUp(self):
@@ -41,9 +43,6 @@ class SubscribeGet(TestCase):
         self.assertIsInstance(form, SubscriptionForm)
 
 
-
-
-
 class SubscribePostValid(TestCase):
     def setUp(self):
         data = dict(name='Henrique Bastos', cpf='12345678901',
@@ -58,6 +57,10 @@ class SubscribePostValid(TestCase):
 
     def test_send_subscribe(self):
         self.assertEqual(1, len(mail.outbox))
+
+
+    def test_save_subscription(self):
+        self.assertTrue(Subscription.objects.exists())
 
 
 class SubscribeInvalidInvalid(TestCase):
@@ -81,6 +84,10 @@ class SubscribeInvalidInvalid(TestCase):
     def test_form_has_erros(self):
         form = self.resp.context['form']
         self.assertTrue(form.errors)
+
+
+    def test_dont_save_subscription(self):
+        self.assertFalse(Subscription.objects.exists())
 
 
 class SubscribeSuccessMessage(TestCase):
