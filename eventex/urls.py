75835550c6517a46cd9d276e248ed18de7ef9137
LeonardoCDP <leonardocottad@gmail.com>
2020-11-17 11:41:37 -0300

    Palestras

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from eventex.core.views import home, speaker_detail
+from eventex.core.views import home, speaker_detail, talk_list
 
 
 urlpatterns = [
@@ -8,7 +8,8 @@ urlpatterns = [
     # url(r'^$', 'eventex.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     path(r'', home, name='home'),
-    path('inscricao/', include('eventex.subscriptions.urls')),
+    path('inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
+    path('palestras/', talk_list, name='talk_list'),
     path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
     path(r'admin/', admin.site.urls),
 ]
